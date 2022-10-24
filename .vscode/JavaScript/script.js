9684372c09c0901aaea14b142959571a4ac8677b
Dashghatage <111383653+Dashghatage@users.noreply.github.com>
2022-10-24 17:52:30 +0530

    first commit

---
+++ .vscode/JavaScript/script.js
@@ -0,0 +1,83 @@
+// console.log(a)
+// var a=10
+
+// // console.log(b)
+// // let b=40
+
+// console.log(d)
+// const d=100
+
+var a=100;
+var b=200;
+
+if (a>b) {
+    document.write(`${a} is greater`)
+} else {
+    document.write(`${b} is greater`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
